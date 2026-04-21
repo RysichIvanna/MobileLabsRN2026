039c0398da42e14cda44c460d0368b6576b2e3a0
Rysich Ivanna <zipz231_rio@student.ztu.edu.ua>
2026-04-21 22:58:39 +0300

    added contact

--- lab2/App.js
+++ lab2/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+import ContactsScreen from './src/screens/ContactsScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
 import MainScreen from './src/screens/MainScreen';
 
@@ -26,6 +27,13 @@ export default function App() {
             component={DetailsScreen}
             options={{
               title: 'Details',
+            }}
+          />
+          <Stack.Screen
+            name="Contacts"
+            component={ContactsScreen}
+            options={{
+              title: 'Contacts',
             }}
           />
         </Stack.Navigator>
